a4981158a0f6445f3297ecbfef6f627d195d1d6f
Stuart Low <stuart@biza.io>
2023-08-01 10:15:30 +1000

    Rename

--- clone-specs.sh
+++ clone-specs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-REPOS="cdrplus-infosec-baseline cdrplus-sharing-arrangement-v1 cdrplus-admission-control cdrplus-enhanced-errors"
+REPOS="datarightplus-infosec-baseline datarightplus-sharing-arrangement-v1 datarightplus-admission-control-baseline datarightplus-error-codes-baseline"
 
 [ ! -d "specs" ] && mkdir specs
 
 for SPEC in $REPOS
   do
-    [ ! -d "specs/$SPEC" ] && git clone git@github.com:cdrplus/$SPEC.git specs/$SPEC
+    [ ! -d "specs/$SPEC" ] && git clone git@github.com:datarightplus/$SPEC.git specs/$SPEC
   done
